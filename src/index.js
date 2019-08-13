375a7640d6f8dfe906d6b384805e7fa006cede60
pnb90 <phuocbui90@gmail.com>
2019-08-13 16:01:06 -0500

    end of day push, added recipes titles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios'
+
+
+axios.defaults.baseURL = 
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
